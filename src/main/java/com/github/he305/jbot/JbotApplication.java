e6345ec234aa688fa4bc9c0634ce853f7a068c93
sergey <he305@mail.ru>
2023-04-20 01:13:40 +0300

    Initial commit

---
+++ src/main/java/com/github/he305/jbot/JbotApplication.java
@@ -0,0 +1,13 @@
+package com.github.he305.jbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/he305/jbot/JbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.he305.jbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
