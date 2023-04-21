f60f133f6ac6294e3dedfa1f81d90ba7be879224
sergey <he305@mail.ru>
2023-04-22 01:24:20 +0300

    loading full context

--- src/test/java/com/github/he305/jbot/user/infra/mapper/UserPostgresMapperTest.java
+++ src/test/java/com/github/he305/jbot/user/infra/mapper/UserPostgresMapperTest.java
@@ -12,17 +12,14 @@ import com.github.he305.jbot.user.infra.data.postgresql.ChatInfoJpa;
 import com.github.he305.jbot.user.infra.data.postgresql.UserInfoJpa;
 import com.github.he305.jbot.user.infra.data.postgresql.UserJpa;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {UserPostgresMapperImpl.class, User.class, UserJpa.class})
+@SpringBootTest
 class UserPostgresMapperTest {
     // private final UserPostgresMapperImpl mapper = new UserPostgresMapperImpl();
 
