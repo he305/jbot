1d208f8793ddebd01f63cfbbdcdca07abc141c05
sergey <he305@mail.ru>
2023-04-23 01:53:03 +0300

    added language labels

--- src/main/java/com/github/he305/jbot/common/enums/AnimeAudioSource.java
+++ src/main/java/com/github/he305/jbot/common/enums/AnimeAudioSource.java
@@ -1,5 +1,11 @@
 package com.github.he305.jbot.common.enums;
 
 public enum AnimeAudioSource {
-    JAPANESE
+    JAPANESE(Language.JAPANESE);
+
+    public final Language label;
+
+    AnimeAudioSource(Language label) {
+        this.label = label;
+    }
 }
