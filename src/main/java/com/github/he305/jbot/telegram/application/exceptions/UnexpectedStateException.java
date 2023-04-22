4cd2f5e2cc2e2f2ca582ddfe727f19c909fb79da
sergey <he305@mail.ru>
2023-04-23 02:34:21 +0300

    removed useless exception

--- src/main/java/com/github/he305/jbot/telegram/application/exceptions/UnexpectedStateException.java
+++
@@ -1,9 +0,0 @@
-package com.github.he305.jbot.telegram.application.exceptions;
-
-import com.github.he305.jbot.common.exceptions.JBotException;
-
-public class UnexpectedStateException extends JBotException {
-    public UnexpectedStateException(String message) {
-        super(message);
-    }
-}
