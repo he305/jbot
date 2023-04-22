c44207260844ffa0a942b90b11bf0613ac14e090
sergey <he305@mail.ru>
2023-04-22 22:33:22 +0300

    changed to PostConstruct init method

--- src/main/java/com/github/he305/jbot/telegram/application/configuration/TelegramBotInitializer.java
+++ src/main/java/com/github/he305/jbot/telegram/application/configuration/TelegramBotInitializer.java
@@ -1,12 +1,16 @@
 package com.github.he305.jbot.telegram.application.configuration;
 
-import org.springframework.beans.factory.InitializingBean;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
 import java.util.List;
 
-public class TelegramBotInitializer implements InitializingBean {
+
+@Configuration
+public class TelegramBotInitializer {
 
     private final TelegramBotsApi telegramBotsApi;
     private final List<LongPollingBot> longPollingBots;
@@ -17,8 +21,8 @@ public class TelegramBotInitializer implements InitializingBean {
         this.longPollingBots = longPollingBots;
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+    @PostConstruct
+    public void afterPropertiesSet() throws TelegramApiException {
         for (LongPollingBot bot : longPollingBots) {
             telegramBotsApi.registerBot(bot);
         }
