1200042143e7c946887c2ac682791a1b9683c17b
sergey <he305@mail.ru>
2023-04-25 03:01:15 +0300

    added GetAnimeDataAction

---
+++ src/main/java/com/github/he305/jbot/telegram/application/actions/GetAnimeDataAction.java
@@ -0,0 +1,42 @@
+package com.github.he305.jbot.telegram.application.actions;
+
+import com.github.he305.jbot.anime.application.factories.AnimeDataProviderFactory;
+import com.github.he305.jbot.anime.application.services.AnimeDataProvider;
+import com.github.he305.jbot.anime.domain.model.AnimeSeries;
+import com.github.he305.jbot.telegram.application.services.GetUserByContextQuery;
+import com.github.he305.jbot.user.domain.model.enums.AnimeListType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.telegram.abilitybots.api.objects.MessageContext;
+
+@Component
+@RequiredArgsConstructor
+public class GetAnimeDataAction implements SingleReplyAction {
+
+    private final GetUserByContextQuery getUserByContextQuery;
+    private final AnimeDataProviderFactory animeDataProviderFactory;
+
+    @Override
+    public String getCommand() {
+        return "get_anime_data";
+    }
+
+    @Override
+    public String getDescription() {
+        return "retrieves anime data from anime list (/get_anime_data anime_name)";
+    }
+
+    @Override
+    public String getMessage(MessageContext context) {
+        String[] argArray = context.arguments();
+        if (argArray.length == 0) {
+            return "Provide anime title (/get_anime_data anime_name)";
+        }
+
+        String animeTitle = String.join(" ", argArray);
+
+        AnimeDataProvider provider = animeDataProviderFactory.getAnimeDataProvider(AnimeListType.MYANIMELIST);
+        AnimeSeries series = provider.getAnimeData(animeTitle);
+        return series.toString();
+    }
+}
